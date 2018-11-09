fc54d2c6cd27e00580a7398c923c315d52998d75
lilelr <lilelr@163.com>
2018-11-09 16:36:55 +0800

    protobuf

---
+++ cmake/protobuf_download.cmake
@@ -0,0 +1,28 @@
+include(ExternalProject)
+
+### protobuf_2_6
+set(protobuf_2_6_URL       https://github.com/protocolbuffers/protobuf/archive/v2.6.1.zip)
+
+set(protobuf_2_6_ROOT ${CMAKE_BINARY_DIR}/thirdparty/protobuf_2_6)
+set(protobuf_2_6_LIB_DIR ${protobuf_2_6_ROOT}/lib)
+set(protobuf_2_6_INCLUDE_DIR ${protobuf_2_6_ROOT}/include)
+message(9 ${protobuf_2_6_ROOT})
+
+#set(glag_configure ${protobuf_2_6_ROOT}/src/protobuf_2_6/configure --prefix=${protobuf_2_6_ROOT})
+set(protobuf_2_6_configure cd ${protobuf_2_6_ROOT}/src/protobuf_2_6 && ./autogen.sh && ./configure --prefix=${protobuf_2_6_ROOT})
+
+set(protobuf_2_6_make cd ${protobuf_2_6_ROOT}/src/protobuf_2_6 && make)
+set(protobuf_2_6_install cd ${protobuf_2_6_ROOT}/src/protobuf_2_6 && make install)
+set(protobuf_2_6_compiler ${protobuf_2_6_ROOT}/src/protobuf_2_6/src/protoc)
+#https://cmake.org/cmake/help/v3.0/module/ExternalProject.html
+ExternalProject_Add(protobuf_2_6
+        URL ${protobuf_2_6_URL} # Full path or URL of source
+        DOWNLOAD_NAME v2.6.1.zip # File name to store (if not end of URL)
+        PREFIX ${protobuf_2_6_ROOT} # Root dir for entire project
+        UPDATE_COMMAND ""
+        PATCH_COMMAND ""
+        CONFIGURE_COMMAND ${protobuf_2_6_configure}  # Build tree configuration command
+        BUILD_COMMAND ${protobuf_2_6_make} # Command to drive the native build
+        INSTALL_COMMAND ${protobuf_2_6_install} # Command to drive install after build
+        #        [INSTALL_DIR dir]           # Installation prefix
+        )
